36efe0e784608746e5b226c104774874e3e5c5b3
ShinCurry <shincurryyang@gmail.com>
2015-12-18 13:20:02 +0800

    CocoaPods & project struct & LoginView.

---
+++ Partime/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  Partime
+//
+//  Created by ShinCurry on 15/12/18.
+//  Copyright © 2015年 ShinCurry. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
