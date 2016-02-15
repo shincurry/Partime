8f48d6bd83f2b73f4f776973b71378b5a82c8a0f
ShinCurry <shincurryyang@gmail.com>
2016-02-15 16:22:25 +0800

    collection view test#2

--- Partime/HomeViewController.swift
+++ Partime/HomeViewController.swift
@@ -119,18 +119,15 @@ extension HomeViewController: UIScrollViewDelegate {
 extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     private func initialCollection() {
-//        homeCollection.delegate = self
         homeCollection.backgroundColor = UIColor.whiteColor()
-        
     }
-    
     
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
         return 2
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return 4
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
@@ -139,11 +136,18 @@ extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSour
         return cell
     }
     
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        if section == 0 {
+            return CGSize.zero
+        } else {
+            return CGSize(width: 50, height: 30)
+        }
+        
+    }
     
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
         let totalWidth = homeCollection.frame.width
         let cellWidth = totalWidth / 2.0 - 1.0
-        print("cellWidth = \(cellWidth)")
         let cellHeight = CGFloat(75)
         return CGSize(width: cellWidth, height: cellHeight)
     }
